bce12bf0294700bad0ecbbb7f2288e0a8bb75b02
ahmedelzahaby <ahmed.elzahaby@bld.ai>
2024-05-09 07:35:52 +0300

    add home upcoming events and deal charts

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,17 @@
+import UpcommingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-charts";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+export {
+    UpcommingEvents,
+    DealsChart, 
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton
+};
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,7 +1,60 @@
+import { DealsChart, UpcommingEvents } from '@/components'
+import { Col, Row } from 'antd'
 import React from 'react'
 
 export const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Row gutter={[32,32]}>
+        <Col
+        xs = {24}
+        sm = {24}
+        xl = {8}
+        >
+          Cards
+        </Col>
+        <Col
+        xs = {24}
+        sm = {24}
+        xl = {8}
+        >
+          Cards
+        </Col>
+        <Col
+        xs = {24}
+        sm = {24}
+        xl = {8}
+        >
+          Cards
+        </Col>
+      </Row>
+      <Row
+      gutter={[32,32]}
+      style={{
+        marginTop: '32px'
+      }}
+      >
+        <Col
+        xs = {24}
+        sm = {24}
+        xl = {8}
+        style={{
+          height: "460px"
+        }}
+        >
+          <UpcommingEvents />
+        </Col>
+        <Col
+        xs = {24}
+        sm = {24}
+        xl = {16}
+        style={{
+          height: "460px"
+        }}
+        >
+          <DealsChart />
+        </Col>
+      </Row>
+    </div>
   )
 }
